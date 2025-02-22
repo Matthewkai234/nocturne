2f235d27e65e3b862d954556683f9316ae0b9886
Matthew <141634353+Matthewkai234@users.noreply.github.com>
2025-02-22 14:06:25 +0300

    initial structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,170 @@
-import Image from "next/image";
+"use client";
+import {
+    Search,
+    User,
+    ShoppingCart,
+    Menu,
+    Swords,
+    X,
+    ChevronDown,
+    Sidebar,
+} from "lucide-react";
+import { useState } from "react";
+import "./styles.css";
+import NextImage from "next/image";
+import NocturnalCover from "../review/NocturnalCover.jpg";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+    const toggleSideBar = () => setIsSideBarOpen((prev) => !prev);
+
+    const [isArrowClicked, setIsArrowClicked] = useState(false);
+    const toggleDropArrow = () => setIsArrowClicked((prev) => !prev);
+
+    return (
+        <>
+            {/* <div className="welcomeDev"><h2>Welcome to our store</h2></div> */}
+
+            <div className="navamp z-60">
+                <div className="navMenu">
+                    {" "}
+                    {isSideBarOpen ? (
+                        <X className="icon" onClick={toggleSideBar} />
+                    ) : (
+                        <Menu className="icon" onClick={toggleSideBar} />
+                    )}
+                </div>
+
+                <div className="brand">
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">Home</a>
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">Store</a>
+                    {/* <Swords className="brandIcon w-8 h-8 text-yellow-400 transform rotate-45 hover:scale-110 transition-transform duration-300 cursor-pointer" /> */}
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">About Us</a>
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">Contact Us</a>
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">FAQ</a>
+                </div>
+
+
+                <div className=" searchCartUserDiv">
+                    <Search className="icon" />
+                    <User className="icon" />
+                    <ShoppingCart className="icon" />
+                </div>
+            </div>
+
+            <div
+                className={`fixed inset-0 top-[3rem] bg-black z-40 bg-opacity-50 transition-opacity duration-300 ${
+                    isSideBarOpen ? "block" : "hidden"
+                }`}
+                onClick={toggleSideBar}
+            ></div>
+
+            <div
+                className={`fixed top-30 left-0 h-full bg-gradient-to-b
+                bg-[#121212]
+                text-white 
+                bg-opacity-95
+                flex
+                flex-col
+                w-64 transform transition-transform duration-300 ease-in-out z-50 ${
+                    isSideBarOpen ? "translate-x-0" : "-translate-x-full"
+                }`}
+            >
+                <div className="sideBarHeaderDiv  justify-center mt-4">
+                    <div className="flex justify-center">
+                        <Swords className="h-10 w-10"/>
+                    </div>
+                    <span className="sideBarHeader flex justify-center font-extrabold text-xl font-sans-serif pt-2">
+                        Our Mystical Products
+                    </span>
+                </div>
+
+                <div className="w-full px-4 my-4">
+                    <hr className="border-t border-gray-600" />
+                </div>
+                
+                <ul className="sidebar-list mt-10 space-y-4">
+                    <li 
+                        className={`group hover:bg-[#7e1b5e] rounded-xl transition-all duration-300 cursor-pointer text-center py-3 px-6 relative flex items-center justify-center space-x-2`}
+                        onClick={toggleDropArrow}
+                    >
+                        <span className="text-lg font-semibold ml-8">A</span>
+                        <span 
+                            className={`transition-transform duration-300 transform ${isArrowClicked ? 'rotate-180' : ''}`}
+                        >
+                            <ChevronDown className="text-white" />
+                        </span>
+                    </li>
+
+                    {isArrowClicked && (
+                        <ul className="sub-list mt-2 ml-8 space-y-2">
+                        <li className="hover:bg-[#7e1b5e] rounded-xl transition-all duration-300 cursor-pointer text-center py-2 px-6">
+                            <span className="text-sm font-medium">a1</span>
+                        </li>
+                        <li className="hover:bg-[#7e1b5e] rounded-xl transition-all duration-300 cursor-pointer text-center py-2 px-6">
+                            <span className="text-sm font-medium">a2</span>
+                        </li>
+                        </ul>
+                    )}
+
+                    <li className="hover:bg-[#7e1b5e] rounded-xl transition duration-500 cursor-pointer text-center py-3 px-6">
+                        <span className="text-lg font-semibold">B</span>
+                    </li>
+                    <li className="hover:bg-[#7e1b5e] rounded-xl transition duration-500 cursor-pointer text-center py-3 px-6">
+                        <span className="text-lg font-semibold">C</span>
+                    </li>
+                    <li className="hover:bg-[#7e1b5e] rounded-xl transition duration-500 cursor-pointer text-center py-3 px-6">
+                        <span className="text-lg font-semibold">D</span>
+                    </li>
+                    <li className="hover:bg-[#7e1b5e] rounded-xl transition duration-500 cursor-pointer text-center py-3 px-6">
+                        <span className="text-lg font-semibold">E</span>
+                    </li>
+                    <li className="hover:bg-[#7e1b5e] rounded-xl transition duration-500 cursor-pointer text-center py-3 px-6">
+                        <span className="text-lg font-semibold">F</span>
+                    </li>
+                </ul>
+
+                <div className="flex-1"></div>
+                <div className="searchCartUserDivInSidebar flex-1">
+                    <Search className="icon" />
+                    <User className="icon" />
+                    <ShoppingCart className="icon" />
+                </div>
+            </div>
+
+            <div className="brandLessThanSmall">
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">Home</a>
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">Store</a>
+                    {/* <Swords className="brandIcon w-8 h-8 text-yellow-400 transform rotate-45 hover:scale-110 transition-transform duration-300 cursor-pointer" /> */}
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">About</a>
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">Contact</a>
+                    <a href="#" className="text-xl font-serif hover:text-[#D4AF37] transition-colors duration-300 cursor-pointer">FAQ</a>
+            </div>
+
+            <div className="grid place-items-center">
+                <NextImage className="nocturneCoverImage w-[40rem] h-[15rem] sm:w-[50rem] sm:h-[15rem] md:w-[70rem] md:h-[18rem] lg:w-[120rem] lg:h-[28rem]" src={NocturnalCover} alt = "Nocturne" />
+            </div>
+
+            <div>
+            </div>
+
+        <footer className="bg-gray-950 text-white py-6">
+            <div className="max-w-screen-xl mx-auto text-center">
+            <p className="text-sm">&copy; 2025 Nocturne. All Rights Reserved.</p>
+            <div className="mt-4">
+            <a href="https://www.facebook.com" target="_blank" className="text-white mx-4 hover:text-blue-600">Facebook</a>
+            <a href="https://www.twitter.com" target="_blank" className="text-white mx-4 hover:text-blue-400">Twitter</a>
+            <a href="https://www.instagram.com" target="_blank" className="text-white mx-4 hover:text-pink-600">Instagram</a>
+            <a href="https://www.linkedin.com" target="_blank" className="text-white mx-4 hover:text-blue-500">LinkedIn</a>
+            </div>
+            <div className="mt-4">
+            <p className="text-sm">Contact us: <a href="mailto:info@yourcompany.com" className="text-white hover:underline">info@yourcompany.com</a></p>
+            </div>
+            </div>
+        </footer>
+        </>
+    );
 }
