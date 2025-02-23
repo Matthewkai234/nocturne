820352faba29e6f357c3c947f69d2dea5d0e6f25
Jamlie <jamlee977@outlook.com>
2025-02-23 19:47:26 +0200

    feat: added a navbar and sidebar to contact-us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,6 +1,9 @@
+import { CompleteNavigators } from "../components/Navbar&Sidebar/Index";
+
 export default function ContactUs() {
     return (
         <>
+            <CompleteNavigators />
             <h1 className="text-3xl">Contact Us</h1>
             <h2 className="text-2xl">Nocturne</h2>
         </>
